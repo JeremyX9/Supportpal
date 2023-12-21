3a27bce6e43b2b999983954f0c27d4189ab271c5
timgebauer <Gebauer@kk-software.de>
2023-12-21 18:25:32 +0100

    simple query of K&K Support Tool on chrome

--- src/index.js
+++ src/index.js
@@ -1,28 +1,54 @@
 jsonData = fetch("./mappings.json").then(response => response.json()).then(data => jsonData = data);
+if (typeof browser === "undefined") {
+    let browser = chrome;
+}
 
-browser.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-    let url = new URL(tabs[0].url);
-    let hostname = url.hostname;
-    if (hostname) {
-        setBasicInformations(hostname);
-        setNameservers(hostname);
-        setDnsRecords(hostname);
+async function bootstrap() {
+    chrome.tabs.query({currentWindow: true, active: true}, function (tabs) {
+        const url = new URL(tabs[0].url)
+        loadData(url.hostname);
+    });
 
-    }
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    bootstrap();
 });
 
+
+async function loadData(url) {
+    const response = await fetch("https://kksupport.ddev.site/api/domainQuery/" + url);
+    if (!response.ok) {
+        return;
+    }
+    const kksupportResponse = await response.text();
+    document.write(kksupportResponse);
+}
+
+
 function setBasicInformations(hostname) {
-    let ipv4 = browser.dns.resolve(hostname, ["disable_ipv6"]).then((value) => { displayElement("ipv4", value.addresses.map(item => `${item} <br>`).join("")); }).catch((error) => { console.log(error); });
-    let ipv6 = browser.dns.resolve(hostname, ["disable_ipv4"]).then((value) => { displayElement("ipv6", value.addresses.map(item => `${item} <br>`).join("")); }).catch((error) => { console.log(error); });
+    let ipv4 = browser.dns.resolve(hostname, ["disable_ipv6"]).then((value) => {
+        displayElement("ipv4", value.addresses.map(item => `${item} <br>`).join(""));
+    }).catch((error) => {
+        console.log(error);
+    });
+    let ipv6 = browser.dns.resolve(hostname, ["disable_ipv4"]).then((value) => {
+        displayElement("ipv6", value.addresses.map(item => `${item} <br>`).join(""));
+    }).catch((error) => {
+        console.log(error);
+    });
     getManager(hostname);
     getCMS(hostname);
 }
+
 function setNameservers(hostname) {
     // set nameservers
 }
+
 function setDnsRecords(hostname) {
     // set dns records
 }
+
 function getManager(hostname) {
     jsonData.then((json) => {
         browser.dns.resolve(hostname, ["disable_ipv6"]).then((value) => {
@@ -36,12 +62,15 @@ function getManager(hostname) {
         })
     });
 }
+
 function getCMS(hostname) {
-    
+
 }
+
 function displayElement(id, text) {
     document.getElementById(id).innerHTML = text;
 }
+
 function displayLink(id, title, url) {
     let manager = document.getElementById(id);
     manager.innerHTML = "";
